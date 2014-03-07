d5e5a4a52070c6e80a6d54fe95f115e08983227d
Weon Yuan <weon.yuan@gmail.com>
2014-03-07 17:09:32 -0500

    Renamed some pictures to remove leading 0s; implemented a refactored photo gallery; loaded most of the buildings content in XML file; some sweet stuffs basically

--- history/scripts/locations.js
+++ history/scripts/locations.js
@@ -1,38 +1,47 @@
-$(document).ready(function(){
-	$(".antiscroll-wrap").antiscroll();
-  	
-	$('ul.tabs').each(function(){
-		// For each set of tabs, we want to keep track of
-		// which tab is active and it's associated content
-		var $active, $content, $links = $(this).find('a');
+if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp = new XMLHttpRequest();
+}
+else { // code for IE6, IE5
+  xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+}
+xmlhttp.open('GET', 'locations.xml', false);
+xmlhttp.send();
+xmlDoc = xmlhttp.responseXML; 
 
-		// If the location.hash matches one of the links, use that as the active tab.
-		// If no match is found, use the first link as the initial active tab.
-		$active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
-		$active.addClass('active');
-		$content = $($active.attr('href'));
+// Read a page's GET URL variables and return them as an associative array.
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
 
-		// Hide the remaining content
-		$links.not($active).each(function () {
-			$($(this).attr('href')).hide();
-		});
+var x = xmlDoc.getElementsByTagName('building');			
+var i = getUrlVars()['id'];
 
-		// Bind the click event handler
-		$(this).on('click', 'a', function(e){
-			// Make the old tab inactive.
-			$active.removeClass('active');
-			$content.hide();
+var name = (x[i].getElementsByTagName('name')[0].childNodes[0].nodeValue);
+var nameLowerCase = x[i].getAttribute('name');
+var date = (x[i].getElementsByTagName('date')[0].childNodes[0].nodeValue);
+var desc = (x[i].getElementsByTagName('desc')[0].childNodes[0].nodeValue);
+var info = (x[i].getElementsByTagName('info')[0].childNodes[0].nodeValue);
+var images = (x[i].getElementsByTagName('images')[0].childNodes[0].nodeValue);
 
-			// Update the variables with the new link and content
-			$active = $(this);
-			$content = $($(this).attr('href'));
+function display() {
+	$('.name').append(name);
+	$('.date').append('Built in ' + date);
+	$('.desc').append(desc);
 
-			// Make the tab active.
-			$active.addClass('active');
-			$content.show();
+	//$('.antiscroll-inner').append('<p>' + info + '</p>');
+	for (var j = 0; j < x[i].getElementsByTagName('info').length; j++) {
+		info = (x[i].getElementsByTagName('info')[j].childNodes[0].nodeValue);
+		$('.antiscroll-inner').append('<p>' + info + '</p>');
+	}
+	
+	$('.thumbnail').append('<img src="../locations/' + nameLowerCase + '/main.jpg">');
+	$('.antiscroll-wrap').antiscroll();
+}
 
-			// Prevent the anchor's default click action
-			e.preventDefault();
-		});
-	});
-});
--- history/scripts/photoGallery.js
+++ history/scripts/photoGallery.js
@@ -4,60 +4,29 @@ $(document).ready(function() {
 	
 	var containerWidth = windowWidth * 0.8;
 	var containerHeight = windowHeight * 0.8;
-	
+
 	loadImage(currentImage);
 	
-	$('.imageContainer').append(nextImage + '/' + numImages);
-		
+	$('#imageContainer').append(nextImage + '/' + numImages);
 });
 
-var images = ['../../locations/greystone/images/01.jpg',
-			  '../../locations/greystone/images/02.jpg',
-			  '../../locations/greystone/images/03.jpg',
-			  '../../locations/greystone/images/04.jpg',
-			  '../../locations/greystone/images/05.jpg',
-			  '../../locations/greystone/images/06.jpg',
-			  '../../locations/greystone/images/07.jpg',
-			  '../../locations/greystone/images/08.jpg',
-			  '../../locations/greystone/images/09.jpg',
-			  '../../locations/greystone/images/10.jpg',
-			  '../../locations/greystone/images/11.jpg',
-			  '../../locations/greystone/images/12.jpg',
-			  '../../locations/greystone/images/13.jpg',
-			  '../../locations/greystone/images/14.jpg',
-			  '../../locations/greystone/images/15.jpg',
-			  '../../locations/greystone/images/16.jpg',
-			  '../../locations/greystone/images/17.jpg',
-			  '../../locations/greystone/images/18.jpg',
-			  '../../locations/greystone/images/19.jpg',
-			  '../../locations/greystone/images/20.jpg',
-			  '../../locations/greystone/images/21.jpg',
-			  '../../locations/greystone/images/22.jpg',
-			  '../../locations/greystone/images/23.jpg',
-			  '../../locations/greystone/images/24.jpg',
-			  '../../locations/greystone/images/25.jpg',
-			  '../../locations/greystone/images/26.jpg',
-			  '../../locations/greystone/images/27.jpg',
-			  '../../locations/greystone/images/28.jpg',
-			  '../../locations/greystone/images/29.jpg',
-			  '../../locations/greystone/images/30.jpg',
-			  '../../locations/greystone/images/31.jpg',
-			  '../../locations/greystone/images/32.jpg',
-			  '../../locations/greystone/images/33.jpg',
-			  '../../locations/greystone/images/34.jpg',
-			  '../../locations/greystone/images/35.jpg',
-			  '../../locations/greystone/images/36.jpg',
-			  '../../locations/greystone/images/37.jpg',
-			  '../../locations/greystone/images/main.jpg'];
+//Prepares the photos array
+function stackPhotos(buildingName, num) {
+	var photos = new Array();
+	for (var i = 0; i < (num - 1); i++) {
+		photos[i] = [buildingName + '/images/' + (i + 1) + '.jpg'];
+	}
+	photos[num - 1] = [buildingName + '/images/main.jpg'];
 
-var numImages = images.length;
+	return photos;
+}
+
+var photos = stackPhotos(nameLowerCase, images);
+
+var numImages = photos.length;
 var previousImage = numImages - 1;
 var currentImage = 0;
 var nextImage = currentImage + 1;
-
-function loadImage(image) {
-	$('.imageContainer').empty().append('<img src="' + photos[image] + '" width="490px" height="60%">' + '<br>');
-}
 
 $(document).keydown(function(e){
     if (e.keyCode == 37) { 
@@ -70,20 +39,28 @@ $(document).keydown(function(e){
     }
 });
 
+function loadImage(image) {
+	$('#imageContainer').empty().append('<img src="' + photos[image] + '" width="490px" height="60%">' + '<br>');
+}
+
 function navigateImage(direction) {
 	if (direction === 0) {
-		//Sets up navigation when user wants to navigate back to last picture
+		//Prepares navigation if user navigates to last picture of the array
 		if (previousImage == 0) {
 			previousImage = numImages - 1;
 			nextImage--;
 			currentImage = nextImage - 1;
 		}
-		//Navigate back to last picture
+		//Navigate back to last picture of the array
 		else if (previousImage >= numImages - 1) {
 			currentImage = previousImage;
 			previousImage = currentImage - 1;
 			nextImage = currentImage + 1;
 		}
+		/*else if (nextImage == numImages || currentImage == numImages) {
+			//do nothing
+		}*/
+		//Navigates to previous picture
 		else {
 			previousImage--;
 			currentImage--;
@@ -109,5 +86,5 @@ function navigateImage(direction) {
 	//Displays the picture
 	loadImage(currentImage);
 
-	$('.imageContainer').append(nextImage + '/' + numImages);
+	$('#imageContainer').append(nextImage + '/' + numImages);
 }
